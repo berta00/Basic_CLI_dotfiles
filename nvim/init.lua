7398310a9d3f8e47d33fe3f195289bdf00fd09ce
berta00 <bertagnollimarco@gmail.com>
2026-01-09 18:13:35 +0100

    simple nvim and tmux config

---
+++ nvim/init.lua
@@ -0,0 +1,30 @@
+
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local keymap = vim.keymap
+
+keymap.set('n', '<M-Left>', '<cmd>TmuxNavigateLeft<cr>')
+keymap.set('n', '<M-Down>', '<cmd>TmuxNavigateDown<cr>')
+keymap.set('n', '<M-Up>', '<cmd>TmuxNavigateUp<cr>')
+keymap.set('n', '<M-Right>', '<cmd>TmuxNavigateRight<cr>')
+
+keymap.set('n', '<leader>\\', '<cmd>split<cr>')
+keymap.set('n', '<leader>|', '<cmd>vsplit<cr>')
+keymap.set('n', '<leader>q', '<cmd>q<cr>')
+
+require("lazy").setup("plugins")
